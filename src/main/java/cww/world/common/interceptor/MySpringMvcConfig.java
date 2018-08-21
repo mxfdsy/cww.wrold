9e31c196a95b2b8938828b7fddbc26c8ecd9ffd3
963559210@qq.com <963559210@qq.com>
2018-08-21 19:58:29 +0800

    拦截器的配置

--- src/main/java/cww/world/common/interceptor/MySpringMvcConfig.java
+++ src/main/java/cww/world/common/interceptor/MySpringMvcConfig.java
@@ -2,7 +2,9 @@ package cww.world.common.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -12,6 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MySpringMvcConfig extends WebMvcConfigurerAdapter {
     @Autowired
     SafeConfig safeConfig;
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/login/login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
 
     /**
      * interceptor配置
